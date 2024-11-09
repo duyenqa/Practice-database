e19d4f4243a4ff8b575bc5c0b4df4fad1a7da5f5
Ngô Thị Kim Duyên <ttemp8399@gmail.com>
2024-11-09 19:50:21 +0700

    Add salary field and update data for employee table

--- db03/candidatedatabase.sql
+++ db03/candidatedatabase.sql
@@ -25,6 +25,7 @@ CREATE TABLE [dbo].[Employee](
 	[Emp_Birthday] DATE,
 	[Emp_City] VARCHAR(30),
 	[Emp_HireDate] DATE,
+	[Emp_Salary] DECIMAL(10,2),
     	[Emp_Email] VARCHAR(255) UNIQUE,
 	[Department_ID] INT FOREIGN KEY REFERENCES Department([Department_ID])
 ) 
@@ -56,16 +57,16 @@ GO
 SELECT * FROM Department
 GO
 
-INSERT INTO Employee ([Emp_ID], [Emp_FirstName], [Emp_LastName], [Emp_Email], [Emp_Birthday], [Emp_HireDate], [Emp_City], [Department_ID])
+INSERT INTO Employee ([Emp_ID], [Emp_FirstName], [Emp_LastName], [Emp_Email], [Emp_Birthday], [Emp_HireDate], [Emp_Salary], [Emp_City], [Department_ID])
 VALUES
-(1, 'John', 'Doe', 'john.doe@example.com','1980-12-25', '2010-01-07','Berlin', 2),
-(2, 'Jane', 'Smith', 'jane.smith@example.com','1993-09-07', '2016-11-08', 'London', 3),
-(3, 'Alice', 'Johnson', 'alice.johnson@example.com','1998-08-20', '2022-11-08', 'San Francisco', 4),
-(4, 'Howell', 'Ervin', 'howell.ervin@example.com','1998-05-25', '2024-05-04', 'San Francisco', 1),
-(5, 'Reichert', 'Glenna', 'reichert.glenna@example.com','1994-09-16', '2024-12-12', 'London', 5),
-(6, 'Amelie', 'Giacoppoli', 'agiacoppoli9@wp.com','1997-11-02', '2023-03-18', 'Madrid', 6),
-(7, 'Richardo', 'Kingzett', 'rkingzett5@dailymotion.com','1996-01-06', '2023-06-09', 'Torino', 6),
-(8, 'Hayden', 'Gillum', 'hgillum2@google.co.jp','1995-04-04', '2020-09-15', 'London', 7)
+(1, 'John', 'Doe', 'john.doe@example.com','1980-12-25', '2010-01-07',35000000,'Berlin', 2),
+(2, 'Jane', 'Smith', 'jane.smith@example.com','1993-09-07', '2016-11-08',21000000, 'London', 3),
+(3, 'Alice', 'Johnson', 'alice.johnson@example.com','1998-08-20', '2022-11-08',18000000, 'San Francisco', 4),
+(4, 'Howell', 'Ervin', 'howell.ervin@example.com','1998-05-25', '2024-05-04',5000000, 'San Francisco', 1),
+(5, 'Reichert', 'Glenna', 'reichert.glenna@example.com','1994-09-16', '2024-02-1',5000000, 'London', 5),
+(6, 'Amelie', 'Giacoppoli', 'agiacoppoli9@wp.com','1997-11-02', '2023-03-18',15000000, 'Madrid', 6),
+(7, 'Richardo', 'Kingzett', 'rkingzett5@dailymotion.com','1996-01-06', '2023-06-09',15000000, 'Torino', 6),
+(8, 'Hayden', 'Gillum', 'hgillum2@google.co.jp','1995-04-04', '2020-09-15',19500000, 'London', 7)
 GO
 SELECT * FROM Employee
 GO
